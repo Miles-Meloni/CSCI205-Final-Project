12dc962fad2b972eefa145979cc4b3ebc16634a9
Keeler Thomas <kpt004@bucknell.edu>
2023-11-30 16:58:09 -0500

    PlayerTest success

--- src/test/java/org/cscigroup3project/MVC/model/PlayerTest.java
+++ src/test/java/org/cscigroup3project/MVC/model/PlayerTest.java
@@ -1,5 +1,6 @@
 package org.cscigroup3project.MVC.model;
 
+import org.cscigroup3project.MVC.model.gameObject.GameObject;
 import org.cscigroup3project.MVC.model.player.Direction;
 import org.cscigroup3project.MVC.model.player.Player;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,7 +17,7 @@ public class PlayerTest {
     @BeforeEach
     public void setUp() {
         // Initialize a new player before each test
-        player = new Player(new int[]{0,0}, "Test", "src/test/resources/cscigroup3project/Sprites/PS");
+        player = new Player(new int[]{0,0}, "Test", "Sprites/PS");
     }
 
     @Test
@@ -28,23 +29,23 @@ public class PlayerTest {
         // Move the player up
         player.move(Direction.UP);
 
-        // Check if the Y position has decreased by 4
+        // Check if the Y position has decreased by the move speed
         assertEquals(initialX, player.getxPos(), DELTA_DOUBLE);
-        assertEquals(initialY - 4, player.getyPos(), DELTA_DOUBLE);
+        assertEquals(initialY - player.getMOVE_SPEED(), player.getyPos(), DELTA_DOUBLE);
 
         // Move the player right
         player.move(Direction.RIGHT);
 
-        // Check if the Y position has decreased by 4 (same as previous)
-        // AND the X position has increased by 4
-        assertEquals(initialX + 4, player.getxPos(), DELTA_DOUBLE);
-        assertEquals(initialY - 4, player.getyPos(), DELTA_DOUBLE);
+        // Check if the Y position has decreased by the move speed (same as previous)
+        // AND the X position has increased by the move speed
+        assertEquals(initialX + player.getMOVE_SPEED(), player.getxPos(), DELTA_DOUBLE);
+        assertEquals(initialY - player.getMOVE_SPEED(), player.getyPos(), DELTA_DOUBLE);
     }
 
     @Test
     public void testPickUpItem() {
         // Create an item
-        Object item = new Object();
+        GameObject item = new GameObject(5,5,15,15,null);
 
         // Pick up the item
         player.pickUpItem(item);
@@ -56,16 +57,16 @@ public class PlayerTest {
     @Test
     public void testGetRidOfItem() {
         // Create an item
-        Object item = new Object();
+        GameObject item = new GameObject(0,0,1,1,null);
 
         // Put the item in the player's inventory
         player.pickUpItem(item);
 
         // Try to get rid of the item
-        boolean removed = player.getRidOfItem(item);
+        GameObject removed = player.dropItem();
 
         // Check if the item was successfully removed
-        assertTrue(removed);
+        assertEquals(removed,item);
         assertFalse(player.getInventory().containsItem(item));
     }
 }
